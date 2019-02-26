11d83da1604b0964703b87b781a2619cf47e83a4
Guldanakys <guldanakys@gmail.com>
2019-02-26 22:08:08 +0600

    main menu upload

---
+++ app/src/main/java/com/example/netapp/HomeFragment.java
@@ -0,0 +1,64 @@
+package com.example.netapp;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
+
+
+public class HomeFragment extends Fragment {
+
+    private ViewFlipper viewFlipper;
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+
+        int images[] = {R.drawable.slide1, R.drawable.slide2, R.drawable.slide3};
+        viewFlipper = (ViewFlipper) v.findViewById(R.id.image_flipper);
+
+        for (int image : images) {
+            flipperImages(image);
+        }
+
+        return v;
+    }
+
+    private void flipperImages(int image) {
+        ImageView imageView = new ImageView(getActivity());
+        imageView.setBackgroundResource(image);
+
+        viewFlipper.addView(imageView);
+        viewFlipper.setFlipInterval(2000);
+        viewFlipper.setAutoStart(true);
+
+        viewFlipper.setInAnimation(getActivity(), android.R.anim.slide_in_left);
+        viewFlipper.setOutAnimation(getActivity(), android.R.anim.slide_out_right);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.search_bar, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+
+    }
+}
---
+++ app/src/main/java/com/example/netapp/NotificationsFragment.java
@@ -0,0 +1,25 @@
+package com.example.netapp;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class NotificationsFragment extends Fragment {
+
+
+    public NotificationsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_notifications, container, false);
+    }
+
+}
